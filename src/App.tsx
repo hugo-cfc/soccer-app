6da47a0937c7314f2dc9871800523081ac80b3e0
Hugo Cesar Firmino Costa <hugo.costa@tceal.tc.br>
2021-09-20 13:43:11 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import api from "./services/api";
+
+import "./style.css";
+
+interface TeamData {
+  position: number;
+  team: {
+    name: string;
+    crestUrl: string;
+  };
+  points: number;
+  playedGames: number;
+  won: number;
+  draw: number;
+  lost: number;
+  goalsFor: number;
+  goalsAgainst: number;
+  goalDifference: number;
+}
 
 function App() {
+  const urlCompetitions = "/v2/competitions/2021/standings";
+  const [table, setTable] = useState([]);
+
+  useEffect(() => {
+    api.get(urlCompetitions).then((response) => {
+      setTable(response.data.standings[0].table);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>Pº</th>
+          <th>Time</th>
+          <th>P</th>
+          <th>J</th>
+          <th>V</th>
+          <th>E</th>
+          <th>D</th>
+          <th>SG</th>
+          <th>GP</th>
+          <th>GC</th>
+        </tr>
+        {table.map((team: TeamData) => (
+          <tr>
+            <td>{team.position}</td>
+            <td>
+              <div>
+                <img src={team.team.crestUrl} alt={team.team.name} />
+                {team.team.name}
+              </div>
+            </td>
+            <td>{team.points}</td>
+            <td>{team.playedGames}</td>
+            <td>{team.won}</td>
+            <td>{team.draw}</td>
+            <td>{team.lost}</td>
+            <td>{team.goalDifference}</td>
+            <td>{team.goalsFor}</td>
+            <td>{team.goalsAgainst}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
---
+++ src/components/TableMatchs/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import api from "../../services/api";
+
+export const TableMatchs: React.FC = () => {
+  return (
+    <>
+      <header>
+        <h1>dawd</h1>
+      </header>
+    </>
+  );
+};
